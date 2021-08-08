ad86906c1eadf8c6ad79515fc0a6261b44aec4ba
roghan-j <82361196+roghan-j@users.noreply.github.com>
2021-08-08 16:57:51 +0530

    ccc

---
+++ basket_app/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.db.models.fields import EmailField
+from django.forms.widgets import PasswordInput
+from basket_app.models import Receiver
+
+class CustomerForm(forms.ModelForm):
+    password= forms.CharField(widget=PasswordInput)
+    # EmailField.name = "jawahar"
+    email = forms.CharField(max_length=256,label="kawaarj")
+    
+
+    class Meta():
+        model= User
+        fields= ('username','password','email')
+
+class OrderForm(forms.ModelForm):
+    class Meta():
+        model= Receiver
+        fields= ('recname','phone','address')
+
+    def __init__(self, *args, **kwargs):
+        super(OrderForm, self).__init__(*args, **kwargs)
+        self.fields['recname'].required = True
+        self.fields['phone'].required = True
+        self.fields['address'].required = True
